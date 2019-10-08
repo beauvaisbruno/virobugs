5cb8f1af99eeec240f066be0b0b2046ac1d433b2
beauvaisbruno@gmail.com <tramoni159>
2019-10-08 06:43:52 +0200

    alpha chanel hide texture

--- android/app/src/main/java/com/virobugs/MainApplication.java
+++ android/app/src/main/java/com/virobugs/MainApplication.java
@@ -3,6 +3,10 @@ package com.virobugs;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.rnscreens.RNScreensPackage;
+import com.swmansion.rnscreens.RNScreensPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import com.swmansion.reanimated.ReanimatedPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -24,6 +28,10 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
               new MainReactPackage(),
+            new RNScreensPackage(),
+            new RNScreensPackage(),
+            new RNGestureHandlerPackage(),
+            new ReanimatedPackage(),
               new ReactViroPackage(ReactViroPackage.ViroPlatform.valueOf(BuildConfig.VR_PLATFORM))
       );
     }
--- index.js
+++ index.js
@@ -1,34 +1,136 @@
-import {AppRegistry, View} from 'react-native';
-import {Viro3DObject, ViroAmbientLight, ViroARScene, ViroARSceneNavigator} from "react-viro";
-import React from "react";
+import {AppRegistry, View, Text} from 'react-native';
+import {Viro3DObject, ViroAmbientLight, ViroARScene, ViroARSceneNavigator, ViroNode, ViroText} from "react-viro";
+import React, {useContext, useEffect, useState} from "react";
+import {createAppContainer, NavigationActions, NavigationContext} from "react-navigation";
+import {createStackNavigator} from 'react-navigation-stack'
 
-const Navigator = ()=>{
-  return  <View style={{ flex:1}}><ViroARSceneNavigator
-    initialScene={{scene:  () => {
-        return  <ViroARScene>
-          <ViroAmbientLight color={"#aaaaaa"} />
+let navRef;
+
+const Navigator0 = () => {
+  const isFocus = useFocusState();
+  console.log("Navigator0 isFocus: ", isFocus);
+  if (!isFocus) {
+    return null;
+  }
+  return <View style={{flex: 1}}><ViroARSceneNavigator
+    initialScene={{
+      scene: () => {
+        return <ViroARScene>
+          <ViroAmbientLight color={"#ffffff"}/>
           <Viro3DObject
-            source={require("./res/outside.glb")}
+            source={require("./res/heavy0.glb")}
             type="GLB"
             position={[0, 0, -1]}
-            onClick={()=> {
-              console.log("Clicked outside");
-            }}
           />
           <Viro3DObject
-            source={require("./res/inside.glb")}
+            source={require("./res/clickable0.glb")}
             type="GLB"
             position={[0, 0, -1]}
-            onClick={()=> {
-              console.log("Clicked inside");
+            onClick={() => {
+              console.log("Clicked Navigator0");
+              navRef.dispatch(NavigationActions.navigate({routeName: "Intermediate0"}));
+              const clear = setTimeout(() => {
+                navRef.dispatch(NavigationActions.navigate({routeName: "Navigator1"}));
+                clearTimeout(clear);
+              }, 3000);
             }}
           />
         </ViroARScene>;
-      }}}
+      }
+    }}
     //https://viromedia.com/signup/
-    apiKey={"SOME_API_KEY"} />
+    apiKey={"SOME_API_KEY"}/>
   </View>;
-}
+};
+const Navigator1 = () => {
+  const isFocus = useFocusState();
+  console.log("Navigator1 isFocus: ", isFocus);
+  if (!isFocus) {
+    return null;
+  }
+  return <View style={{flex: 1}}><ViroARSceneNavigator
+    initialScene={{
+      scene: () => {
+        return <ViroARScene>
+          <ViroAmbientLight color={"#ffffff"}/>
+          <Viro3DObject
+            source={require("./res/heavy1.glb")}
+            type="GLB"
+            position={[0, 0, -1]}
+          />
+          <Viro3DObject
+            source={require("./res/clickable1.glb")}
+            type="GLB"
+            position={[0, 0, -1]}
+            onClick={() => {
+              console.log("Clicked Navigator1");
+              navRef.dispatch(NavigationActions.navigate({routeName: "Intermediate1"}));
+              const clear = setTimeout(() => {
+                navRef.dispatch(NavigationActions.navigate({routeName: "Navigator0"}));
+                clearTimeout(clear);
+              }, 3000);
+            }}
+          />
+        </ViroARScene>;
+      }
+    }}
+    //https://viromedia.com/signup/
+    apiKey={"SOME_API_KEY"}/>
+  </View>;
+};
 
-AppRegistry.registerComponent('virobugs', () => Navigator);
+export function useFocusState() {
+  const navigation = useContext(NavigationContext);
+  const [focusState, setFocusState] = useState(navigation.isFocused() ? "didFocus" : "didBlur");
+console.log("navigation: ",navigation.state);
+  const handleEvt = ({type:newState}) => {
+    console.log("handleEvt, newState: ", newState);
+    if (focusState === newState)
+      return;
+    const clear = setTimeout(() => {
+      setFocusState(newState);
+      clearTimeout(clear);
+    }, 100);
+  };
+  useEffect(
+    () => {
+      const subsDF = navigation.addListener("didFocus", handleEvt);
+      const subsDB = navigation.addListener("willBlur", handleEvt);
+      return () => {
+        subsDF.remove();
+        subsDB.remove();
+      };
+    },
+    [navigation.state.key]
+  );
+  if (focusState === "willBlur")
+    return false;
+  if (focusState === "didFocus")
+    return true;
+  console.error("unknown state: ", focusState);
+  return true;
+}
+const Intermediate0 = ()=>{
+  return <View><Text>Intermediate0</Text></View>;
+  }
+const Intermediate1 = ()=>{
+  return <View><Text>Intermediate1</Text></View>;
+}
+const StackNavigation = createStackNavigator({
+  "Navigator0": Navigator0,
+  "Intermediate0": Intermediate0,
+  "Navigator1": Navigator1,
+  "Intermediate1": Intermediate1,
+  "Navigator2": Navigator0,
+}, {
+  initialRouteName: "Navigator0",
+  headerMode: "none",
+  defaultNavigationOptions: {
+    gesturesEnabled: false
+  },
+});
+
+const NavigationContainer = createAppContainer(StackNavigation);
+
+AppRegistry.registerComponent('virobugs', () => () => <NavigationContainer ref={ ref => {navRef = ref;}} />);
 
