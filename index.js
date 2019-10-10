87e1b5b4f78c3cd43f3a08d6979909745fb97eb4
beauvaisbruno@gmail.com <tramoni159>
2019-10-10 12:36:07 +0200

    Craesh for Virocamera on UI Constraint

--- index.js
+++ index.js
@@ -1,34 +1,25 @@
 import {AppRegistry, View} from 'react-native';
-import {Viro3DObject, ViroAmbientLight, ViroARScene, ViroARSceneNavigator} from "react-viro";
+import {Viro3DObject, ViroAmbientLight, ViroARScene, ViroARSceneNavigator,ViroCamera} from "react-viro";
 import React from "react";
 
 const Navigator = ()=>{
   return  <View style={{ flex:1}}><ViroARSceneNavigator
     initialScene={{scene:  () => {
         return  <ViroARScene>
+          <ViroCamera position={[0,0,0]} active={true} >
           <ViroAmbientLight color={"#aaaaaa"} />
           <Viro3DObject
-            source={require("./res/outside.glb")}
+            source={require("./res/object.glb")}
             type="GLB"
             position={[0, 0, -1]}
-            onClick={()=> {
-              console.log("Clicked outside");
-            }}
           />
-          <Viro3DObject
-            source={require("./res/inside.glb")}
-            type="GLB"
-            position={[0, 0, -1]}
-            onClick={()=> {
-              console.log("Clicked inside");
-            }}
-          />
+          </ViroCamera>
         </ViroARScene>;
       }}}
     //https://viromedia.com/signup/
-    apiKey={"SOME_API_KEY"} />
+    apiKey={"BDF01DAC-4F97-4D5D-8C8A-DD8C609019B1"} />
   </View>;
-}
+};
 
 AppRegistry.registerComponent('virobugs', () => Navigator);
 
